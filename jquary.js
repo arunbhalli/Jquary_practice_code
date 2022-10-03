6361dac05a43bf3b13ff0d01e104fb7a69d74ac8
arunbhalli <tomarbhalli@gmail.com>
2022-10-03 09:34:53 +0200

    plays around with id of tag

--- jquary.js
+++ jquary.js
@@ -1,6 +1,6 @@
 // selectors
 $(document).ready(function () {
   $('button').click(function () {
-    $('p').css('background-color', 'blue');
+    $('#p2').css('background-color', 'blue');
   });
 });
